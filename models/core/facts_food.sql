d01514d617a313655b8e1ed98b28ae2d3022337e
Daniel Rigney <daniel.y.rigney@gmail.com>
2023-03-29 02:58:31 +0000

    model update to include brand in fact_food table

--- models/core/facts_food.sql
+++ models/core/facts_food.sql
@@ -6,21 +6,35 @@ with food_data as (
 ), 
 
 dim_names as (
-    select * from {{ ref('dim_food_names') }}
-)
+    select * 
+    from {{ ref('dim_food_names') }}
+),
 
 dim_nutrients as (
-    select * from {{ ref('dim_nutrient_codes') }}
-)
+    select * 
+    from {{ ref('dim_nutrient_codes') }}
+),
 
 dim_brands as (
-    select * from {{ ref('dim_brands') }}
+    select * 
+    from {{ ref('dim_brands') }}
 )
 
 select 
+food_data.fdc_id, 
+dim_names.description,
+dim_brands.brand_owner,
+dim_brands.brand_name,   
+dim_nutrients.name, 
+food_data.amount,
+dim_nutrients.unit_name
+
+
    
 from food_data
-inner join dim_zones
-on trips_unioned.pickup_locationid = pickup_zone.locationid
-inner join dim_zones
-on trips_unioned.dropoff_locationid = dropoff_zone.locationid
+inner join dim_names
+on food_data.fdc_id = dim_names.fdc_id
+inner join dim_nutrients
+on food_data.nutrient_id = dim_nutrients.id
+inner join dim_brands
+on food_data.fdc_id = dim_brands.fdc_id
